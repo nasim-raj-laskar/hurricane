859a4a016dbaed360c37fa2b94db1e9043800026
nasim-raj-laskar <144606891+nasim-raj-laskar@users.noreply.github.com>
2025-10-24 17:04:54 +0530

    Training Script Tested

--- research/train.py
+++ research/train.py
@@ -1,19 +1,21 @@
+# Libraries
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'                                                                                            # Suppress TensorFlow logging
+import tensorflow as tf             
 from tensorflow.keras import Sequential,Input                                                                                       # type: ignore
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, SeparableConv2D, GlobalAveragePooling2D, Rescaling        # type: ignore
 from tensorflow.keras.callbacks import EarlyStopping                                                                                # type: ignore
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning)
+import warnings                                                                                                                     # Suppress warnings
+warnings.filterwarnings("ignore", category=UserWarning)                                                                             
 
 # Paths
 dirs = {
-    "train": r'X:\nasim_xhqpjmy\Code\MLops\hurricane-damage\dataset\train_another',
-    "val": r'X:\nasim_xhqpjmy\Code\MLops\hurricane-damage\dataset\validation_another',
-    "test": r'X:\nasim_xhqpjmy\Code\MLops\hurricane-damage\dataset\test'
+    "train": r'X:\nasim_xhqpjmy\Code\MLops\hurricane-damage\dataset\train_another',                                                 #train dataset path
+    "val": r'X:\nasim_xhqpjmy\Code\MLops\hurricane-damage\dataset\validation_another',                                              #validation dataset path
+    "test": r'X:\nasim_xhqpjmy\Code\MLops\hurricane-damage\dataset\test'                                                            #test dataset path
 }
 
+# Parameters
 IMG_SIZE = (128, 128)
 BATCH = 32
 
@@ -25,8 +27,8 @@ train_ds, val_ds, test_ds = map(load_ds, [dirs["train"], dirs["val"], dirs["test
 
 # Model
 model = Sequential([
-    Input(shape=(96, 96, 3)),
-    Rescaling(1./255, input_shape=(96, 96, 3)),
+    Input(shape=(128, 128, 3)),
+    Rescaling(1./255, input_shape=(128, 128, 3)),
     Conv2D(8, 3, activation='relu', padding='same'),
     MaxPooling2D(),
     Conv2D(16, 3, activation='relu', padding='same'),
@@ -40,9 +42,14 @@ model = Sequential([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
 
-# Train + Evaluate
+# Train 
 history = model.fit(train_ds, validation_data=val_ds, epochs=5, 
                     callbacks=[EarlyStopping(patience=3, restore_best_weights=True)])
 
+# Evaluate
 loss, acc = model.evaluate(test_ds)
 print(f"Test Accuracy: {acc*100:.2f}%")
+
+# # Save model
+# model.save("hurricane.h5")
+# print("Model saved as hurricane.h5")
