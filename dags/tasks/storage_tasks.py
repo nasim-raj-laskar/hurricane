fffb95a14449d837f5d7a442766dfe314912b9c8
nasim-raj-laskar <144606891+nasim-raj-laskar@users.noreply.github.com>
2025-11-08 15:59:18 +0530

    S3 creds updated

--- dags/tasks/storage_tasks.py
+++ dags/tasks/storage_tasks.py
@@ -17,6 +17,15 @@ def save_model_s3(**context):
     os.environ['MLFLOW_TRACKING_PASSWORD'] = mlflow_conn.password
     mlflow.set_tracking_uri(mlflow_conn.extra_dejson['tracking_uri'])
 
+    # Get AWS credentials from Airflow Connection
+    aws_conn = BaseHook.get_connection('aws_default')
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=aws_conn.login,
+        aws_secret_access_key=aws_conn.password,
+        region_name=aws_conn.extra_dejson.get('region_name', 'eu-north-1')
+    )
+
     # Prepare model file
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     tmp_model = '/tmp/trained_model.h5'
@@ -28,7 +37,6 @@ def save_model_s3(**context):
         raise FileNotFoundError(f"{tmp_model} not found before upload!")
 
     # Upload to S3
-    s3 = boto3.client('s3')
     s3.upload_file(renamed_path, S3_BUCKET, f"models/hurricane_{timestamp}.h5")
 
     s3_uri = f"s3://{S3_BUCKET}/models/hurricane_{timestamp}.h5"
